58b7812c3c0638be6bcad0f7548c69ebcf021ec3
Sam Liu <ontouchstart@gmail.com>
2026-01-29 10:51:03 -0500

    test-bin crate

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//!
+//! ``` 
+//! assert_true::test();
+//! test_bin::test();
+//! ```
+//!
+pub fn test() {
+    assert_true::test();
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+//!
+//! ``` 
+//! assert_true::test();
+//! ```
+//!
+fn main() {
+    assert_true::test();
+}
