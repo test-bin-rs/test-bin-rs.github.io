7d8fb68b0ab2833caa9ab7cc2255cf4d278a9b3e
Sam Liu <ontouchstart@gmail.com>
2026-01-29 11:09:03 -0500

    update docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,21 @@
-//! This crate is used by `src/main.rs` and `examples/main.rs`
-//!
 //! ```
 //! assert_true::test();
 //! ```
+//!
+//! This crate is used by `src/main.rs` for the `assert_true` bin
+//! that you can run via
+//! ```text
+//! cargo install \
+//!     --git https://github.com/test-bin-rs/test-bin-rs.github.io \
+//!     --branch assert-true \
+//!     --root /tmp/.cargo 
+//! /tmp/.cargo/bin/assert-true
+//! ```
+//!
+//! (Read it carefully before you copy and paste to run it on your computer.) 
+//!
+//! Then you can delete the `/tmp/.cargo` directory via `rm -rf /tmp/.cargo` 	
+//!
 //!
 
 mod core;
